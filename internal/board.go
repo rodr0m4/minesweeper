523e8b3dcea4483285426c3cf11d410f8c7d65c5
rodroma <rodrigoleonardomartin@gmail.com>
2020-12-15 22:18:44 -0300

    Board

---
+++ internal/board.go
@@ -0,0 +1,141 @@
+package internal
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+)
+
+type Board struct {
+	matrix  [][]*Tile
+	rows    int
+	columns int
+}
+
+type Position struct {
+	Row    int
+	Column int
+}
+
+func NewBoard(rows, columns, bombs int) *Board {
+	matrix := newMatrix(rows, columns)
+
+	for _, position := range bombPositions(rows, columns, bombs) {
+		matrix[position.Row][position.Column].hasBomb = true
+	}
+
+	return &Board{
+		matrix:  matrix,
+		rows:    rows,
+		columns: columns,
+	}
+}
+
+func (b Board) HasRows(i int) bool {
+	return b.rows == i
+}
+
+func (b Board) HasColumns(i int) bool {
+	return b.columns == i
+}
+
+func (b Board) Area() int {
+	return b.rows * b.columns
+}
+
+func (b Board) HasArea(i int) bool {
+	return b.Area() == i
+}
+
+func (b Board) Find(position Position) *Tile {
+	row := position.Row
+	column := position.Column
+
+	if b.isOutOfBounds(row, column) {
+		log.Panic(b.outOfBoundsError(row, column))
+	}
+
+	return b.matrix[row][column]
+}
+
+func (b Board) FindScalar(rows int, columns int) *Tile {
+	if b.rows <= rows || b.columns <= columns {
+		panic("invalid access")
+	}
+
+	return b.matrix[rows][columns]
+}
+
+func (b Board) Traverse(f func(*Tile, Position)) {
+	traverse(b.rows, b.columns, func(position Position) {
+		tile := b.Find(position)
+		f(tile, position)
+	})
+}
+
+func (b Board) Position(row, column int) (Position, error) {
+	if b.isOutOfBounds(row, column) {
+		return Position{}, b.outOfBoundsError(row, column)
+	}
+
+	return Position{Row: row, Column: column}, nil
+}
+
+// Private members
+
+func newMatrix(rows int, columns int) [][]*Tile {
+	matrix := make([][]*Tile, rows)
+	for i := 0; i < rows; i++ {
+		matrix[i] = make([]*Tile, columns)
+
+		for j := 0; j < columns; j++ {
+			matrix[i][j] = &Tile{}
+		}
+	}
+	return matrix
+}
+
+// TODO: Optimize
+// Naive algorithm
+func bombPositions(rows, columns, bombs int) []Position {
+	var positions []Position
+
+	for i := 0; i < bombs-1; i++ {
+		found := true
+		for found {
+			newPosition := Position{
+				Row:    rand.Intn(rows),
+				Column: rand.Intn(columns),
+			}
+
+			for _, position := range positions {
+				found = position == newPosition
+				if found {
+					break
+				}
+			}
+
+			if len(positions) == 0 || !found {
+				positions = append(positions, newPosition)
+			}
+		}
+	}
+
+	return positions
+}
+
+func traverse(rows, columns int, f func(Position)) {
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			f(Position{Row: i, Column: j})
+		}
+	}
+}
+
+func (b Board) isOutOfBounds(row int, column int) bool {
+	return row < 0 || column < 0 || b.rows <= row || b.columns <= column
+}
+
+func (b Board) outOfBoundsError(row int, column int) error {
+	return fmt.Errorf("invalid position access (%d, %d) when Board is %dx%d, use Board::Position for creating a valid Position", row, column, b.rows, b.columns)
+}
---
+++ internal/board_test.go
@@ -0,0 +1,87 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_A_Board_Is_Created_With_The_Given_Rows_And_Columns(t *testing.T) {
+	board := NewBoard(10, 10, 0)
+
+	assert.True(t, board.HasRows(10))
+	assert.True(t, board.HasColumns(10))
+
+	assert.False(t, board.HasRows(5))
+	assert.False(t, board.HasColumns(5))
+}
+
+func Test_A_Board_Has_An_Area(t *testing.T) {
+	board := NewBoard(5, 5, 0)
+
+	assert.True(t, board.HasArea(25))
+	assert.False(t, board.HasArea(100))
+}
+
+func Test_Board_Position_For_Valid_Positions(t *testing.T) {
+	var position Position
+	var err error
+
+	board := NewBoard(10, 10, 0)
+
+	position, err = board.Position(0, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, Position{Row: 0, Column: 0}, position)
+
+	position, err = board.Position(2, 4)
+	assert.NoError(t, err)
+	assert.Equal(t, Position{Row: 2, Column: 4}, position)
+
+	position, err = board.Position(9, 9)
+	assert.NoError(t, err)
+	assert.Equal(t, Position{Row: 9, Column: 9}, position)
+}
+
+func Test_Board_Position_For_Invalid_Positions(t *testing.T) {
+	var err error
+
+	board := NewBoard(10, 10, 0)
+
+	_, err = board.Position(-1, -1)
+	assert.Error(t, err)
+
+	_, err = board.Position(0, 10)
+	assert.Error(t, err)
+
+	_, err = board.Position(5, 324)
+	assert.Error(t, err)
+}
+
+func Test_Find_On_Empty_Board(t *testing.T) {
+	board := NewBoard(10, 10, 0)
+
+	tile := board.Find(Position{}) // We find the tile on the top-left
+
+	assert.False(t, tile.HasBomb(), "this board does not have bombs")
+	assert.Equal(t, HiddenTile, tile.State(), "every tile in a new board should be hidden")
+}
+
+func Test_Find_On_A_Board_With_Bombs(t *testing.T) {
+	expectedBombs := 2
+	board := NewBoard(2, 2, expectedBombs)
+	expectedNotBombs := board.Area() - expectedBombs
+
+	var bombs int
+	var notBombs int
+
+	board.Traverse(func(tile *Tile, _ Position) {
+		if tile.hasBomb {
+			bombs++
+		} else {
+			notBombs++
+		}
+	})
+
+	assert.Equal(t, expectedBombs, bombs, "should have %d bombs, found %d", expectedBombs, bombs)
+	assert.Equal(t, expectedNotBombs, notBombs, "should have %d not-bombs, found %d", expectedNotBombs, notBombs)
+}
