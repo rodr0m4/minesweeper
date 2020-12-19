801bac4bc412d8120834f412fbe9cabd05c38f7d
rodroma <rodrigoleonardomartin@gmail.com>
2020-12-19 17:25:08 -0300

    register route

--- internal/platform/rest/routes.go
+++ internal/platform/rest/routes.go
@@ -4,7 +4,9 @@ func (s *Server) RegisterRoutes() {
 	r := s.Engine
 	startGameHandler := s.StartGameHandler
 	showGameHandler := s.ShowGameHandler
+	tapHandler := s.TapHandler
 
 	r.POST("/games", startGameHandler.StartGame)
 	r.GET("/games", showGameHandler.ShowGame)
+	r.PATCH("/games", tapHandler.Tap)
 }
